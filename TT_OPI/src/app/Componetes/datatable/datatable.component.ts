3f28517d9a5623a51b2993037fae510131571481
CarlosGG03 <carlosgarzon969@gmail.com>
2022-07-18 15:59:59 -0500

    Programav1.4

--- TT_OPI/src/app/Componetes/datatable/datatable.component.ts
+++ TT_OPI/src/app/Componetes/datatable/datatable.component.ts
@@ -1,32 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
-
+import data from 'src/app/Componetes/datatable/Data.json'
 @Component({
   selector: 'app-datatable',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.css']
 })
-export class DatatableComponent implements OnDestroy,OnInit {
+export class DatatableComponent{
   
-
+  Data: any = data;
   dtOptions: DataTables.Settings = {};
   dtTrigger = new Subject<any>();
    data: any;
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 2
-    };
-    this.http.get('https://github.com/CarlosGG03/CGG_CODE/blob/master/TT_OPI/src/assets/Data.json')
-      .subscribe(console.log);
-  }
-
-
-  ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
-  }
 
 }
